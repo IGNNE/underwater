902983dae5464bd8decd491368c1ed5aed12234f
IGNNE <30634883+IGNNE@users.noreply.github.com>
2020-07-12 18:37:33 +0200

    init

---
+++ components/wateroc2.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('wateroc2', {
+    schema: {},
+    init: function () {},
+    update: function () {},
+    tick: function () {},
+    remove: function () {},
+    pause: function () {},
+    play: function () {}
+  });
