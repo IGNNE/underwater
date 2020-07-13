3a1740c7eef32fdbe66f29469b34394bfbc356c6
IGNNE <30634883+IGNNE@users.noreply.github.com>
2020-07-13 20:16:18 +0200

    Movement controls, minor fixes, wip of own implementation

--- components/water.js
+++ components/water.js
@@ -1,17 +1,22 @@
+/**
+ * WAframe wrapper around the jbouny / three.js Water
+ * @author IGNNE
+ */
+
 import { Water } from '../lib/three.js/Water.js';
 
 AFRAME.registerComponent('water', {
     water: null,
     schema: {
-        waterColor: {type: 'int', default: 0x001E0F},
-        sunColor: {type: 'int', default: 0xFFFFFF}
+        waterColor: { type: 'int', default: 0x001E0F },
+        sunColor: { type: 'int', default: 0xFFFFFF }
     },
     init: function () {
         // get basic elements for three.js
         let scene = document.querySelector('a-scene').object3D;
         let renderer = document.querySelector('a-scene').systems["renderer"];
         let camera = document.querySelector('a-camera').object3D;
-        
+
         let waterGeometry = new THREE.PlaneBufferGeometry(10000, 10000);
         this.water = new Water(
             waterGeometry,
@@ -28,15 +33,15 @@ AFRAME.registerComponent('water', {
                 fog: scene.fog !== undefined
             }
         );
-        // flip the "surface" -> TODO 
+        // flip the "surface" to point upside down -> TODO make configurable (or even better, make it work with position/rotation)
         this.water.rotation.x = Math.PI / 2;
         scene.add(this.water);
     },
     update: function () {
         // TODO
-     },
+    },
     tick: function (time, timeDelta) {
-        this.water.material.uniforms[ 'time' ].value += timeDelta / 1000 // HACK
+        this.water.material.uniforms['time'].value += timeDelta / 1000 // use the real timeDelta
     },
     remove: function () { },
     pause: function () { },
